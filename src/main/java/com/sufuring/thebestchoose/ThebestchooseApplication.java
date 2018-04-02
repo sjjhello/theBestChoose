90cb2debc381d653bf700a8dbfaebe74be1f028e
MsI <sjj_hello@163.com>
2018-04-02 13:24:38 +0800

    "首次提交"

---
+++ src/main/java/com/sufuring/thebestchoose/ThebestchooseApplication.java
@@ -0,0 +1,12 @@
+package com.sufuring.thebestchoose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThebestchooseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThebestchooseApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sufuring/thebestchoose/ThebestchooseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sufuring.thebestchoose;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThebestchooseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
